eba9c7e8658308f8e1b3b58e88e607a634a91420
Grigore Stefan <g_stefan@yahoo.com>
2021-03-15 12:37:38 +0200

    project-structure

--- build/ubuntu.clean.sh
+++ build/ubuntu.clean.sh
@@ -5,6 +5,6 @@
 
 echo "-> clean quantum-script-extension-shell"
 
-rm -rf bin
-rm -rf build
+rm -rf output
+rm -rf temp
 rm -rf lib
--- build/ubuntu.make.sh
+++ build/ubuntu.make.sh
@@ -17,5 +17,5 @@ cmdX(){
 	fi
 }
 
-cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-shell.static.compile
-cmdX xyo-cc --mode=$ACTION @util/quantum-script-extension-shell.dynamic.compile
+cmdX xyo-cc --mode=$ACTION @build/source/quantum-script-extension-shell.static.compile
+cmdX xyo-cc --mode=$ACTION @build/source/quantum-script-extension-shell.dynamic.compile
